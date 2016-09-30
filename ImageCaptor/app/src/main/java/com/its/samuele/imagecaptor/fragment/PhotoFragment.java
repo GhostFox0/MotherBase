13a3a65589aec7d3efc85e4f968d62b4f1372de4
Samuele <samuele.pretto@gmail.com>
2016-09-30 17:31:43 +0200

    sa

---
+++ ImageCaptor/app/src/main/java/com/its/samuele/imagecaptor/fragment/PhotoFragment.java
@@ -0,0 +1,99 @@
+package com.its.samuele.imagecaptor.fragment;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.its.samuele.imagecaptor.R;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_OK;
+
+/**
+ * Created by Samuele.Pretto on 30/09/2016.
+ */
+
+public class PhotoFragment extends Fragment {
+
+    FloatingActionButton fab;
+    ImageView showPhoto;
+    Uri file;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.photo_fragment, container, false);
+        fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        showPhoto = (ImageView) view.findViewById(R.id.photoVisual);
+
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            fab.setEnabled(false);
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},0);
+        }
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0){
+            if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                fab.setEnabled(true);
+            }
+        }
+    }
+
+    public void takePhoto(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        file = Uri.fromFile(getOutputMediaFile());
+
+        startActivityForResult(intent,100);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100){
+            if (resultCode == RESULT_OK){
+                showPhoto.setImageURI(file);
+            }
+        }
+    }
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Camera");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+
+        String timeStampo = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath()+ File.separator+"IMG_"+timeStampo +".jpg");
+    }
+}
