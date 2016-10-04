81b347b1c5c8ff161e51eaeec9a39aa6320cf623
Samuele <samuele.pretto@gmail.com>
2016-10-04 11:54:28 +0200

    upgrade

--- ImageCaptor/app/src/main/java/com/its/samuele/imagecaptor/fragment/PhotoFragment.java
+++ ImageCaptor/app/src/main/java/com/its/samuele/imagecaptor/fragment/PhotoFragment.java
@@ -1,15 +1,20 @@
 package com.its.samuele.imagecaptor.fragment;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
@@ -26,6 +31,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static android.app.Activity.RESULT_OK;
+import static android.support.v4.content.PermissionChecker.checkSelfPermission;
 
 /**
  * Created by Samuele.Pretto on 30/09/2016.
@@ -36,6 +42,7 @@ public class PhotoFragment extends Fragment {
     FloatingActionButton fab;
     ImageView showPhoto;
     Uri file;
+    Bitmap bPhoto;
 
     @Nullable
     @Override
@@ -44,32 +51,34 @@ public class PhotoFragment extends Fragment {
         fab = (FloatingActionButton) view.findViewById(R.id.fab);
         showPhoto = (ImageView) view.findViewById(R.id.photoVisual);
 
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-            fab.setEnabled(false);
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},0);
-        }
-
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                takePhoto(v);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},001);
+                }
             }
         });
 
         return view;
     }
 
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == 0){
-            if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
-                fab.setEnabled(true);
+        super.onRequestPermissionsResult(requestCode, permissions,grantResults);
+        if (requestCode == 0001){
+            if (ContextCompat.checkSelfPermission(getContext(),Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getContext(),Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getContext(),Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                takePhoto();
+            }
+            else {
+                noPermission();
             }
         }
     }
 
-    public void takePhoto(View view){
+    public void takePhoto(){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         file = Uri.fromFile(getOutputMediaFile());
 
@@ -80,7 +89,8 @@ public class PhotoFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 100){
             if (resultCode == RESULT_OK){
-                showPhoto.setImageURI(file);
+                bPhoto = (Bitmap) data.getExtras().get("data");
+                showPhoto.setImageBitmap(bPhoto);
             }
         }
     }
@@ -94,6 +104,23 @@ public class PhotoFragment extends Fragment {
         }
 
         String timeStampo = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        return new File(mediaStorageDir.getPath()+ File.separator+"IMG_"+timeStampo +".jpg");
+        return new File(mediaStorageDir.getAbsolutePath()+ File.separator+"IMG_"+timeStampo +".jpg");
+    }
+
+    public void noPermission(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+        alert.setTitle("Attenzione");
+        alert.setMessage("Per usare l'app è necessario modificare i permessi");
+        alert.setIcon(R.mipmap.ic_launcher);
+        alert.setPositiveButton("Permessi", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getActivity().getPackageName(),null);
+                intent.setData(uri);
+                startActivityForResult(intent, 234, null);
+            }
+        });
+        alert.create().show();
     }
 }
